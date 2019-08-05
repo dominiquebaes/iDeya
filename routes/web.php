9f789cc9230bd1b0fba3cabb76106ab9a0707852
dominiquebaes <dominiquejustine.baes@g.msuiit.edu.ph>
2019-08-05 17:03:43 +0800

    grouped routes in web

--- routes/web.php
+++ routes/web.php
@@ -37,10 +37,6 @@ Route::group(["prefix"=>'events'], function(){
 
 // should be grouped with the events above
 Route::group(['middleware' => 'auth'], function(){
-    Route::group(['namespace' => 'Dashboard'], function(){
-        Route::view('/dashboard',"dashboard.index")->name('dashboard.index');
-    });
-
 	Route::group(['prefix' => 'inventory'], function(){
         Route::group(['namespace' => 'Inventory'], function(){
             Route::post('/',"AddChairController")->name('inventory.chair.add');
